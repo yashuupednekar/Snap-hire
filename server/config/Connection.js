42f3bcfc6c27811d6d76a4dab80b7d8bfbdfcfd4
yashuupednekar <yashpednekar89@gmail.com>
2025-11-24 10:46:46 +0530

    changes in config/Connetion.js

--- server/config/Connection.js
+++ server/config/Connection.js
@@ -5,14 +5,11 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.error("MongoDB Connection Failed:", error.message);
-        process.exit(1); // Exit process with failure
+        process.exit(1);
     }
 };
 
